fe91d97131dd7fd02cb801769fe6208cd14e6789
sp0gg <joe.nelson@runbox.com>
2016-02-19 17:33:10 -0500

    refactor

--- lib/cyclic_rotation.rb
+++ lib/cyclic_rotation.rb
@@ -1,10 +1,7 @@
 class CyclicRotation
   def self.solution a, k
     return [] if a.empty?
-    k.times do
-      e = a.pop
-      a.unshift(e)
-    end
-    a
+    k = k % a.size
+    a.rotate! -k
   end
 end
--- spec/lib/cyclic_rotation_spec.rb
+++ spec/lib/cyclic_rotation_spec.rb
@@ -5,8 +5,11 @@ describe CyclicRotation do
   describe '#solution' do
     it 'should rotate arrays' do
       expect(CyclicRotation.solution([3, 8, 9, 7, 6], 1)).to eq [6, 3, 8, 9, 7]
+      expect(CyclicRotation.solution([3, 8, 9, 7, 6], 21)).to eq [6, 3, 8, 9, 7]
       expect(CyclicRotation.solution([3, 8, 9, 7, 6], 3)).to eq [9, 7, 6, 3, 8]
       expect(CyclicRotation.solution([], 1)).to eq []
+      expect(CyclicRotation.solution([], 100000)).to eq []
+      expect(CyclicRotation.solution([5], 100000)).to eq [5]
     end
   end
 end
